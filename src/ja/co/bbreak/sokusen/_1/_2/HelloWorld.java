06fdc9beeea12c55193deac13a23d0bfac13c867
FukayaHiroki <asitanonegif@gmail.com>
2019-09-24 14:02:45 +0900

    Initial commit

---
+++ src/ja/co/bbreak/sokusen/_1/_2/HelloWorld.java
@@ -0,0 +1,10 @@
+package ja.co.bbreak.sokusen._1._2;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWorld");
+	}
+
+}
